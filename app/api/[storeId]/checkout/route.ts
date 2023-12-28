2b5d2c09e4ce82d023511fd73c49b79afbc2ab4d
Antra Verma <antraverma99@gmail.com>
2023-12-28 19:13:13 +0530

    updated success url

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -70,8 +70,8 @@ export async function POST(
     phone_number_collection: {
       enabled: true,
     },
-    success_url: `https://market-x-store.vercel.app/cart?success=1`,
-    cancel_url: `https://market-x-store.vercel.app/cart?canceled=1`,
+    success_url: `${process.env.NEXT_APP_FRONTEND_STORE_URL}/cart?success=1`,
+    cancel_url: `${process.env.NEXT_APP_FRONTEND_STORE_URL}/cart?canceled=1`,
     metadata: {
       orderId: order.id,
     },
